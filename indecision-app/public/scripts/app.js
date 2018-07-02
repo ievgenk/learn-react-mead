ff87f7d2ecfa65ab3d362208fb23ff49bfb56bbd
Evgeny Kasian <ekasian@outlook.com>
2018-07-02 11:53:38 +0200

    started to learn React

---
+++ indecision-app/public/scripts/app.js
@@ -0,0 +1,9 @@
+console.log('App.js is running')
+
+// JSX - Stands for Javascript XML. Javascript extension
+
+
+let appRoot = document.getElementById('#app')
+let template = <p>This is JSX from app.js</p>
+
+ReactDOM.render(template, appRoot)
